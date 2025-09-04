dc017f2208e1d5f2b7ce71be54b632a7a84f4654
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-04 22:00:01 +0200

    😕 I guess I didn't run datagen

--- src/main/java/com/mrbysco/npcinvasion/data/NPCDatagen.java
+++ src/main/java/com/mrbysco/npcinvasion/data/NPCDatagen.java
@@ -6,7 +6,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber
 public class NPCDatagen {
 	@SubscribeEvent
 	public static void gatherData(GatherDataEvent.Client event) {
